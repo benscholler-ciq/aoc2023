c789b43794ca8ddc55931a57312cd7e8e10a659b
Ben Scholler <ben.scholler@captivateiq.com>
2023-12-01 14:26:27 -0500

    Digits replacement for 2nd part.

--- 1/1.php
+++ 1/1.php
@@ -3,8 +3,12 @@
 $file = fopen('input.txt', 'r');
 $sum = 0;
 
+$nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+$replace = ['zero0zero', 'one1one', 'two2two', 'three3three', 'four4four', 'five5five', 'six6six', 'seven7seven', 'eight8eight', 'nine9nine'];
+
 while (!feof($file)) {
-    preg_match_all('/[0-9]/', fgets($file, 4096), $matches); # Read line into buffer, regex to find numbers
+    $line = str_replace($nums, $replace, fgets($file, 4096)); # Digits replacement
+    preg_match_all('/[0-9]/', $line, $matches); # Read line into buffer, regex to find numbers
     if (count($matches) > 0) { # If there are numbers
         $first = $matches[0][0]; # First number from matches
         $last = array_slice($matches[0], -1, 1)[0]; # Last number
