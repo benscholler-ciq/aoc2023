29a7f46a3a6b7e3e245fbf1e68d8163603b7a44b
Ben Scholler <ben.scholler@captivateiq.com>
2023-12-01 14:06:36 -0500

    Oopsies.

--- 1/1.php
+++ 1/1.php
@@ -2,12 +2,13 @@
 
 $file = fopen('input.txt', 'r');
 $sum = 0;
+
 while (!feof($file)) { # Read line into buffer
     preg_match_all('/[0-9]/', fgets($file, 4096), $matches); # Regex to find numbers
     if (count($matches) > 0) { # If there are numbers
-        $first = array_shift($matches[0]); # Pop off first number from matches
-        $last = (count($matches[0]) > 0) ? array_slice($matches[0], -1, 1)[0] : ''; # Get last number if it exists and not first element
-        $sum += "{$first}{$last}"; # Sum up
+        $first = intval($matches[0][0]); # First number from matches
+        $last = intval(array_slice($matches[0], -1, 1)[0]); # Last number
+        $sum += intval("{$first}{$last}"); # Sum up
     }
 }
 
