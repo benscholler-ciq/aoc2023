43b5cba01408b3367a46a4ca9d5973872dd38ad9
Ben Scholler <ben.scholler@captivateiq.com>
2023-12-01 14:18:13 -0500

    Unnecessary intval calls.

--- 1/1.php
+++ 1/1.php
@@ -3,11 +3,11 @@
 $file = fopen('input.txt', 'r');
 $sum = 0;
 
-while (!feof($file)) { # Read line into buffer
-    preg_match_all('/[0-9]/', fgets($file, 4096), $matches); # Regex to find numbers
+while (!feof($file)) {
+    preg_match_all('/[0-9]/', fgets($file, 4096), $matches); # Read line into buffer, regex to find numbers
     if (count($matches) > 0) { # If there are numbers
-        $first = intval($matches[0][0]); # First number from matches
-        $last = intval(array_slice($matches[0], -1, 1)[0]); # Last number
+        $first = $matches[0][0]; # First number from matches
+        $last = array_slice($matches[0], -1, 1)[0]; # Last number
         $sum += intval("{$first}{$last}"); # Sum up
     }
 }
