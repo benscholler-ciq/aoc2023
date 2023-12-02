8ee4a82de3a016f889e98003418af2ae01b2bfa3
Ben Scholler <ben.scholler@captivateiq.com>
2023-12-02 01:08:36 -0500

    Even more optimizing.

--- 2/2.php
+++ 2/2.php
@@ -7,10 +7,9 @@ while (!feof($file)) {
     $line = fgets($file, 4096);
     $sub = 1;
     foreach (['red', 'green', 'blue'] as $color) {
-        $count = preg_match_all("/(\d+)\s$color/", $line, $matches);
-        $sub = $sub * max(array_map('intval', $matches[1]));
+        preg_match_all("/(\d+)\s$color/", $line, $matches);
+        $sub *= max($matches[1]);
     }
-
     $sum += $sub;
 }
 
