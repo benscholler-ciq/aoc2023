b4e0dd5eda198a54bf44056a749d7736cc2aed50
Ben Scholler <ben.scholler@captivateiq.com>
2023-12-02 01:01:37 -0500

    Optimize for fun.

--- 2/2.php
+++ 2/2.php
@@ -4,20 +4,14 @@ $file = fopen('input.txt', 'r');
 $sum = 0;
 
 while (!feof($file)) {
-    $line = fgets($file, 4096); # Read file
-    $roundStart = strpos($line, ':');
-    $gameNum = preg_match('/Game\s(\d+)\:/', $line, $gameMatch);
+    $line = fgets($file, 4096);
+    $sub = 1;
+    foreach (['red', 'green', 'blue'] as $color) {
+        $count = preg_match_all("/(\d+)\s$color/", $line, $matches);
+        $sub = $sub * max(array_map('intval', $matches[1]));
+    }
 
-    $redCount = preg_match_all('/(\d+)\sred/', $line, $redMatches);
-    $blueCount = preg_match_all('/(\d+)\sblue/', $line, $blueMatches);
-    $greenCount = preg_match_all('/(\d+)\sgreen/', $line, $greenMatches);
-
-    $maxRedCount = max(array_map('intval', $redMatches[1]));
-    $maxBlueCount = max(array_map('intval', $blueMatches[1]));
-    $maxGreenCount = max(array_map('intval', $greenMatches[1]));
-
-    $power = $maxRedCount * $maxBlueCount * $maxGreenCount;
-    $sum += $power;
+    $sum += $sub;
 }
 
 echo $sum;
